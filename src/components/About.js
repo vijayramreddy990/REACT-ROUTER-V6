aac1387a4923575878040d5385022cbc7ee1472e
vijay <vijayramreddy990@gmail.com>
2022-05-01 20:56:04 +0530

    usage of Link tag

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div>
+    <div className="App">
       <h1>About Page</h1>
+      <p>This is the About page of our awsome app</p>
+      <p>Here we are learning about react router 6</p>
+      <Link to="/">Go to Home Page</Link>
     </div>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
+    <div className="App">
       <h1>Home Page</h1>
+      <p>This is the home page of our awsome app</p>
+      <p>Here we are learning about react router 6</p>
+      <Link to="/about">Go to About Page</Link>
     </div>
   );
 };
